8f16de04992044d0530d7021881e1a99c486c3a2
Stefan Thoss <stefanthoss@users.noreply.github.com>
2019-05-30 10:13:53 -0700

    Reformatted Python code

--- calculate-geo-center.py
+++ calculate-geo-center.py
@@ -1,23 +1,29 @@
-import time
 import sys
+import time
 
-import pandas as pd
 import geopy
+import pandas as pd
 
-DEFAULT_TIMEOUT = 20        # Default timeout when contacting OpenStreetMap in seconds
-WAIT_BETWEEN_REQUESTS = 1.2 # Wait time between OpenStreetMap requests in seconds
+DEFAULT_TIMEOUT = 20  # Default timeout when contacting OpenStreetMap in seconds
+WAIT_BETWEEN_REQUESTS = 1.2  # Wait time between OpenStreetMap requests in seconds
+
 
 def get_coordinates(geolocator, address):
     try:
         location = geolocator.geocode(address, timeout=DEFAULT_TIMEOUT)
+        return location.latitude, location.longitude
     except geopy.exc.GeocoderTimedOut as e:
-        sys.exit(f"Error, the address {address} couldn't be converted to coordinates because of {e.msg}.")
-    return location.latitude, location.longitude
+        sys.exit(
+            f"Error, the address {address} couldn't be converted to coordinates because of {e.msg}."
+        )
 
 
 def calc_weighted_center(weighted_df):
-    return (weighted_df["lat"] * weighted_df["weight"]).sum() / weighted_df["weight"].sum(), \
-           (weighted_df["lng"] * weighted_df["weight"]).sum() / weighted_df["weight"].sum()
+    return (
+        (weighted_df["lat"] * weighted_df["weight"]).sum() / weighted_df["weight"].sum(),
+        (weighted_df["lng"] * weighted_df["weight"]).sum() / weighted_df["weight"].sum(),
+    )
+
 
 def main():
     geolocator = geopy.Nominatim(user_agent="weighted-geo-center")
@@ -33,9 +39,7 @@ def main():
     print("\nData Input:\n", data)
 
     for i, row in data.iterrows():
-        lat, lng = get_coordinates(geolocator, row["address"])
-        data.at[i, "lat"] = lat
-        data.at[i, "lng"] = lng
+        data.at[i, "lat"], data.at[i, "lng"] = get_coordinates(geolocator, row["address"])
         time.sleep(WAIT_BETWEEN_REQUESTS)
 
     print("\nData w/ Geo Coordinates:\n", data)
@@ -46,5 +50,5 @@ def main():
     print(f"The weighted geographic center is: {geo_center}")
     print(f"Open in Google Maps: http://www.google.com/maps/place/{geo_center[0]},{geo_center[1]}")
 
-main()
 
+main()
